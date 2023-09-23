1fe448400d4d4b16f5216712c5e809221538b60b
Woidyla <142675593+Woidyla@users.noreply.github.com>
2023-09-23 03:49:25 +0000

    adding script

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,4 +1,4 @@
-const body = document.querySelector('.body');
+const body = document.querySelector('body');
 const modeToggle = document.getElementById('mode-toggle');
 const modeStatus = document.querySelector('.mode-status');
 
